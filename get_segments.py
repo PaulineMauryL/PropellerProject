7d802c12f494893277f205a6d48885be61f9d7de
PaulineMauryL <paulinemaurylaribiere@gmail.com>
2018-10-11 22:10:18 +0200

    solved upper propeller point, divided get_segments in get_segments and get_blade

--- get_segments.py
+++ get_segments.py
@@ -16,27 +16,41 @@ def blade_alone(propeller_coords, vect_upper, middle_point, dmiddle):
     return upper_blade, lower_blade
 
 
-def get_segments(blade, dmiddle, d_max, vect, nb_seg):
-    delta_d = (d_max - dmiddle)/nb_seg
-    
-    last_plane = np.append(vect, dmiddle)
-    
-    #new_plane = np.array([0,0,0,0])
-    segments = {}
-    segments["points"] = []
-    
-    for i in range(nb_seg):
-        index_segment = []
-        new_plane = last_plane[:] + [0,0,0,delta_d]
+def get_planes(blade, dmiddle, d_max, vect, nb_seg):
+	delta_d = (d_max - dmiddle)/nb_seg
+	last_plane = np.append(vect, dmiddle)
+	planes = [last_plane]
+
+	for i in range(nb_seg):
+		new_plane = last_plane[:] + [0,0,0,delta_d]
+		planes.append(new_plane)
+		last_plane = new_plane
+
+	return planes
+
+
+
+def get_segments(blade, planes, nb_seg):
+	segments = {}
+	segments["points"] = []
+
+	for i in range(nb_seg):
+		index_segment = []
+
+		for index, point in blade.iterrows():
+			point_mult = np.append(point, 1)
+
+			if(point_mult @ planes[i] > 0 and point_mult @ planes[i+1] <= 0):
+				index_segment.append(index)
         
-        for index, point in blade.iterrows():
-            point_mult = np.append(point, 1)         
-            if(point_mult @ last_plane > 0 and point_mult @ new_plane < 0):
-                index_segment.append(index)
-        
-        last_plane = new_plane
-        
-        segments["points"].append(blade.loc[index_segment].copy().as_matrix())
-        
-    return segments
-    
+		segments["points"].append(blade.loc[index_segment].copy().as_matrix())
+
+	for index, point in blade.iterrows():
+		point_mult = np.append(point, 1)
+
+		if(point_mult @ planes[nb_seg] > 0):
+			index_segment.append(index) 
+			
+	segments["points"].append(blade.loc[index_segment].copy().as_matrix())
+	
+	return segments
--- test.py
+++ test.py
@@ -1,25 +1,28 @@
-from plot_prop import plot_eigenvectors, plot_pointcloud
+from plot_prop import plot_direction, plot_pointcloud, plot_segments
 from prop_info import extreme_points, blade_info
-from get_segments import blade_alone, get_segments
+from get_segments import blade_alone, get_segments, get_planes
+from major_axis import get_major_axis
 import pandas as pd
 import numpy as np
 
 # read dataframe
 propeller_coords = pd.read_csv('propeller_data.csv')
-
 max_point, min_point, middle_point = extreme_points(propeller_coords)
-
-vect_upper, vect_lower, dmiddle, dmax, dmin = blade_info(max_point, min_point, middle_point)
-
+vect_upper, vect_lower, vect_blade, dmiddle, dmax, dmin = blade_info(max_point, min_point, middle_point)
 upper_blade, lower_blade = blade_alone(propeller_coords, vect_upper, middle_point, dmiddle)
+vect_out, vect_side = get_major_axis(propeller_coords, middle_point, vect_blade)
 
+
+#plot_direction(propeller_coords, vect_blade, vect_out, vect_side)
+#plot_pointcloud(lower_blade)
+#plot_eigenvectors(propeller_coords, vect_upper)
+nb_seg = 5
+planes = get_planes(upper_blade, dmiddle, dmax, vect_upper, nb_seg)
+segments = get_segments(upper_blade, planes, nb_seg)
+#print(segments)
+plot_segments(segments)
 #plot_pointcloud(propeller_coords)
-plot_eigenvectors(propeller_coords, vect_upper)
 '''
-segments = get_segments(upper_blade, dmiddle, dmax, vect_upper, 4)
-
-#plot_pointcloud(propeller_coords)
-
 for i, array in enumerate(segments["points"]):
     print(i)
     print(array)
