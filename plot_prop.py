785f1a457df1fa4569cc916ccea6edc3e6607867
PaulineMauryL <paulinemaurylaribiere@gmail.com>
2018-10-19 21:34:52 +0200

    projection of couple point on plan

--- plot_prop.py
+++ plot_prop.py
@@ -141,7 +141,9 @@ def plot_segments(segments):
 
 	plt.show()
 
-def plot_projection(df_u, df_d):
+    
+    
+def plot_projection_up_down(df_u, df_d):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot(df_d["X"], df_d["Y"], df_d["Z"], 'co', markersize=3, alpha=0.2)
@@ -167,6 +169,19 @@ def plot_all_projections(proj_up, proj_down):
         ax.plot(proj_down[elem]["X"], proj_down[elem]["Y"], proj_down[elem]["Z"], 'go', markersize=3, alpha=0.2)
     for elem in proj_up:
         ax.plot(proj_up[elem]["X"], proj_up[elem]["Y"], proj_up[elem]["Z"], 'ro', markersize=3, alpha=0.2)
+    ax.set_xlabel('x_values', fontsize=15)
+    ax.set_ylabel('y_values', fontsize=15)
+    ax.set_zlabel('z_values', fontsize=15)
+    plt.title('Projections on plane', fontsize=20)
+    plt.show()
+    
+    
+    
+def plot_final_projections(projections):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for elem in projections:
+        ax.plot(projections[elem][0], projections[elem][1], projections[elem][2], 'go', markersize=3, alpha=0.2)
     ax.set_xlabel('x_values', fontsize=15)
     ax.set_ylabel('y_values', fontsize=15)
     ax.set_zlabel('z_values', fontsize=15)
