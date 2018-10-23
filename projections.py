03b663c4291db5d512e4f0d82deacf393db16592
PaulineMauryL <paulinemaurylaribiere@gmail.com>
2018-10-23 10:06:46 +0200

    projection bug sloved

--- projections.py
+++ projections.py
@@ -5,16 +5,17 @@ from myMathFunction import distance_point_plane, project_point, distance_p2p, po
 
 #Plane i is associated with segments i-1 and i
 def projections_by_side(nb_seg, planes, segments, nb_point):
-    proj_down = {}
-    proj_up = {}
+    proj_down = []
+    proj_up = []
     
     idx_up = []
     idx_down = []
 
     for proj in range(1, nb_seg): #1,2,3,4
         df_d, df_u, idx_down_proj, idx_up_proj = project_on_plane(planes[proj], segments['points'][proj-1], segments['points'][proj], nb_point)
-        proj_down[proj-1] = df_d
-        proj_up[proj-1] = df_u
+        print(proj)
+        proj_down.append(df_d)
+        proj_up.append(df_u)
         
         idx_down.append(idx_down_proj) #0,1,2,3
         idx_up.append(idx_up_proj)
@@ -57,14 +58,19 @@ def project_on_plane(plan, segment_down, segment_up, nb_point):
     
     return df_d, df_u, sort_idx_down, sort_idx_up
 
+
+
 def find_closest_couple_plane(proj_down, proj_up):
     couple = []
+    down = []
+    up = []
+    
     d = np.zeros([proj_down.shape[0],proj_up.shape[0]])
     #print("find_closest_couple_plane")
     for i, x1 in proj_down.iterrows():
         for j, x2 in proj_up.iterrows():
             d[i][j] = distance_p2p(x1, x2)
-            
+    
     min_down = np.argmin(d, axis=0)
     min_up = np.argmin(d, axis=1)
 
@@ -75,17 +81,26 @@ def find_closest_couple_plane(proj_down, proj_up):
         if(min_up[value]==i):
             #print("min_up[value] is {}".format(min_up[value]))
             couple.append((i,value))
+            down.append(proj_down.iloc[i])
+            up.append(proj_up.iloc[value])
             
-    return couple
+    return couple, down, up
+
+
 
 def couple_all_planes(proj_down, proj_up, nb_seg):
     #print("couple all plane \n \n \n")
     couples = []
+    down = []
+    up = []
+    
     for proj in range(nb_seg-1):
-        print(proj)
-        couples.append( find_closest_couple_plane(proj_down[proj], proj_up[proj]) )
-    return couples
-
+        #print(proj)
+        couple, down_proj, up_proj = find_closest_couple_plane(proj_down[proj], proj_up[proj])
+        couples.append(couple)
+        down.append(down_proj)
+        up.append(up_proj)
+    return couples, down, up
 
 
 def project_couple(plane, proj_up, proj_down): 
